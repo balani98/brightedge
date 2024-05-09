dd3443eba0fe303ea3293c9b3463e80a4f2b1449
Deepanshu Balani <deepanshu.balani@brainlabsdigital.com>
2024-05-09 18:19:19 +0530

    configs added in invoker

--- invoker.py
+++ invoker.py
@@ -9,7 +9,28 @@ import datetime
 import time
 from logger import setup_logging
 import logging
+import json
+import ast
+import configparser
+Config = configparser.ConfigParser()
+Config.read('config.ini')
+SERVICE_ACCOUNT = Config.get('GENERAL', 'SERVICE_ACCOUNT')
+PROJECT_ID = Config.get('GENERAL','PROJECT_ID')
+TABLE_ID = Config.get('GENERAL','TABLE_ID')
+USERNAME = Config.get('GENERAL','USERNAME')
+PASSWORD = Config.get('GENERAL','PASSWORD')
+SLACK_TOKEN =  Config.get('GENERAL','SLACK_TOKEN')
+ACCOUNT_ID =  Config.get('GENERAL','ACCOUNT_ID')
+ACCOUNTS = Config.get("GENERAL", "ACCOUNTS")
+def get_account_name():
+    print(ACCOUNTS)
+    accounts_json = json.loads(ast.literal_eval(ACCOUNTS))
+    account_id_str = str(ACCOUNT_ID)
+    account = accounts_json[account_id_str]
+    return account
+
 def invoke_the_API():
+    account_name = get_account_name()
     i=1
     file_logger = setup_logging('Logs/'+ account_name + '/' + account_name + '_'+'overall_logs.log', logging.DEBUG)
     start_date= datetime.datetime(2023, 1, 1)
